336d839b50812cd3e5586ebcfb4e17d620e8678d
Kel_Seduc <kelabrantes.me@gmail.com>
2025-01-23 09:16:24 -0300

    Inclusão da imagem menuHamburgue.svg

--- src/components/GameHome.jsx
+++ src/components/GameHome.jsx
@@ -2,6 +2,7 @@
 
 import { useContext, useEffect, useState } from "react"
 import { GoogleContext } from "@/context/GoogleContext"
+import Image from "next/image"
 
 export default function GameHome(){
 
@@ -13,12 +14,25 @@ export default function GameHome(){
     useEffect(()=>{
         const nameSession = sessionStorage.getItem('@AuthWithGoogle:name')
         setName(nameSession)
+
     },[])
 
 
 
     return(
             <div className="bg-colorprimary h-1/4 p-2 ">
+                <div>
+                    <div>
+                        <Image
+                        src='/menuHamburguer.svg'
+                        height={30}
+                        width={30}
+                        alt="icone Menu"
+                        />
+                    </div>
+                </div>
+
+
                 <p className="text-white">Bem vindo: {name} </p>
                 <p className="text-white" onClick={exitGoogleLogin}>Sair</p>
             </div>
