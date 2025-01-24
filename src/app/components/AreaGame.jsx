aed08f7b7ec5aff53b0d5184c634c5889098c3b1
Kel_Seduc <kelabrantes.me@gmail.com>
2025-01-24 08:40:43 -0300

    inclusão dos botões de volta

--- src/app/components/AreaGame.jsx
+++ src/app/components/AreaGame.jsx
@@ -5,7 +5,6 @@ export default async function AreaGame(){
 
     const Data = await Consult()
 
-    console.log(Data[0])
 
 
     return(
--- src/app/components/TextoCard.jsx
+++ src/app/components/TextoCard.jsx
@@ -1,25 +1,40 @@
 'use client'
 
-import { useState } from "react"
+import { useState, useRef } from "react"
+import Link from "next/link"
 
 
 export default function TextoCard({data}){
 
     const [values, setValue] = useState(0)
+    const [button, setButton] = useState('Plus')
+    const refButton = useRef()
+    const refButtonPlus = useRef()
+
 
     function plusCard(){
-        setValue(values + 1 )
-
-        if(values >= 2){
-            setValue(0)
+        if(values < 2){
+            setValue(values + 1 )
+        }else{
+            refButton.current.classList.toggle('hidden')
+            refButtonPlus.current.classList.toggle('hidden')
         }
+ 
+
+        // if(values >=2){
+        //     setButton('')
+        //     setValue(0)
+        // }
     }
 
 
+
+
     return(
-        <div className="-bgslate-600 flex flex-col">
+        <div className=" flex flex-col">
             {data[values].per}
-            <button onClick={plusCard}>plu</button>
+            <button ref={refButtonPlus}  onClick={plusCard} className="">{button}</button>
+            <Link href='/game'><button ref={refButton} className="hidden">Voltar</button></Link>
         </div>
         
 
