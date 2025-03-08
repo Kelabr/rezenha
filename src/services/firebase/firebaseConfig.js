e7e31f0c907e825a48a15b51682faa0ec68e2bcb
Kel <144748800+Kelabr@users.noreply.github.com>
2025-03-08 19:17:18 -0300

    Update firebaseConfig.js

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -2,15 +2,6 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAcTKUwQ9tOThL4beO7gpzDDS-Bk5_9Wzs",
-  authDomain: "rezenha-280c1.firebaseapp.com",
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKETD,
-  messagingSenderId: process.env.MESSAGIN_SENDER_ID,
-  appId: process.env.APP_ID,
-};
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 const db = getFirestore(app)
